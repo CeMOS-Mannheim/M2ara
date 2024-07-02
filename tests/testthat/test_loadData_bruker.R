7872ad654df24fcff3ebe95b79622901f049adb2
thomas-enzlein <thomas.enzlein@gmail.com>
2024-07-02 08:00:42 +0200

    again

--- tests/testthat/test_loadData_bruker.R
+++ tests/testthat/test_loadData_bruker.R
@@ -5,18 +5,9 @@ test_that("data can be loaded and processed from Bruker Flex",
               format(Sys.time(), "%H:%M:%OS")
             }
             setwd(dirname(common::file.find(pattern = "req.txt")))
-<<<<<<< HEAD
 
             # download test data and unzip
             curl::curl_download("https://figshare.com/ndownloader/files/46156791", "testdata_bruker.zip")
-=======
-            cat("Working directory is ", getwd(), "\n")
-
-            # download test data and unzip
-            cat(timeNow(), "downloading Bruker Flex test data...\n")
-            curl::curl_download("https://figshare.com/ndownloader/files/46156791", "testdata_bruker.zip")
-            cat(timeNow(), "unzip Bruker test data...\n")
->>>>>>> main
             unzip("testdata_bruker.zip")
 
             # overwrite settings
@@ -29,17 +20,12 @@ test_that("data can be loaded and processed from Bruker Flex",
 
             library(shinytest2)
             options(shiny.testmode = TRUE)
-<<<<<<< HEAD
 
-=======
-            cat(timeNow(), "Starting app...\n")
->>>>>>> main
             app <- AppDriver$new(app_dir = getwd(),
                                  name = "M2ara_bruker load test",
                                  seed = 42,
                                  timeout = 4.5*1e5)
 
-<<<<<<< HEAD
             app$click("load")
             Sys.sleep(120)
             app$click("process")
@@ -47,21 +33,6 @@ test_that("data can be loaded and processed from Bruker Flex",
 
             expect_equal(exp$export$isSpectrumList, TRUE)
             expect_equal(exp$export$numSpec, 88)
-=======
-            cat(timeNow(), "loading spectra...\n")
-            app$click("load")
-            Sys.sleep(120)
-            cat(timeNow(), "processing...\n")
-            app$click("process")
-            cat(timeNow(), "done!\n")
-            exp <-app$get_values(export = TRUE)
-
-            cat(timeNow(), "running tests...\n")
-            expect_equal(exp$export$isSpectrumList, TRUE)
-            expect_equal(exp$export$numSpec, 88)
-
-            cat(timeNow(), "Sucess! Cleaning up...\n")
->>>>>>> main
             fs::dir_delete("Curve")
             fs::file_delete("settings.csv")
             fs::file_delete("testdata_bruker.zip")
