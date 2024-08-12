c3d4fe01da63c7474233d6c14a330b9b727d4404
thomas-enzlein <thomas.enzlein@gmail.com>
2024-08-12 09:37:50 +0200

    - found another bug. try testing again

--- tests/testthat/test_loadData_bruker.R
+++ tests/testthat/test_loadData_bruker.R
@@ -1,49 +1,49 @@
-# library(testthat)
-#
-# test_that("data can be loaded and processed from Bruker Flex",
-#           {
-#
-#             timeNow <- function() {
-#               format(Sys.time(), "%H:%M:%OS")
-#             }
-#             setwd(dirname(common::file.find(pattern = "req.txt")))
-#
-#             # download test data and unzip
-#             curl::curl_download("https://figshare.com/ndownloader/files/46156791", "testdata_bruker.zip")
-#             if(!file.exists("testdata_bruker.zip")) {
-#               stop("Downloading testdata_bruker.zip failed.")
-#             }
-#             unzip("testdata_bruker.zip")
-#
-#
-#
-#             # overwrite settings
-#             if(!file.exists("tests/testthat/settings_bruker_data.csv")) {
-#               stop("Could not find settings_bruker_data.csv.\n")
-#             }
-#             fs::file_copy(path = "tests/testthat/settings_bruker_data.csv",
-#                           new_path = "settings.csv",
-#                           overwrite = TRUE)
-#
-#             library(shinytest2)
-#             options(shiny.testmode = TRUE)
-#
-#             app <- AppDriver$new(app_dir = getwd(),
-#                                  name = "M2ara_bruker load test",
-#                                  seed = 42,
-#                                  timeout = 10000,
-#                                  wait = TRUE)
-#
-#             app$click("load")
-#             app$wait_for_idle(timeout = 300*1000)
-#             app$click("process")
-#             app$wait_for_idle(timeout = 300*1000)
-#             exp <-app$get_values(export = TRUE)
-#
-#             expect_equal(exp$export$isSpectrumList, TRUE)
-#             expect_equal(exp$export$numSpec, 88)
-#             fs::dir_delete("Curve")
-#             fs::file_delete("settings.csv")
-#             fs::file_delete("testdata_bruker.zip")
-#           })
-#
+library(testthat)
+
+test_that("data can be loaded and processed from Bruker Flex",
+          {
+
+            timeNow <- function() {
+              format(Sys.time(), "%H:%M:%OS")
+            }
+            setwd(dirname(common::file.find(pattern = "req.txt")))
+
+            # download test data and unzip
+            curl::curl_download("https://figshare.com/ndownloader/files/46156791", "testdata_bruker.zip")
+            if(!file.exists("testdata_bruker.zip")) {
+              stop("Downloading testdata_bruker.zip failed.")
+            }
+            unzip("testdata_bruker.zip")
+
+
+
+            # overwrite settings
+            if(!file.exists("tests/testthat/settings_bruker_data.csv")) {
+              stop("Could not find settings_bruker_data.csv.\n")
+            }
+            fs::file_copy(path = "tests/testthat/settings_bruker_data.csv",
+                          new_path = "settings.csv",
+                          overwrite = TRUE)
+
+            library(shinytest2)
+            options(shiny.testmode = TRUE)
+
+            app <- AppDriver$new(app_dir = getwd(),
+                                 name = "M2ara_bruker load test",
+                                 seed = 42,
+                                 timeout = 10000,
+                                 wait = TRUE)
+
+            app$click("load")
+            app$wait_for_idle(timeout = 300*1000)
+            app$click("process")
+            app$wait_for_idle(timeout = 300*1000)
+            exp <-app$get_values(export = TRUE)
+
+            expect_equal(exp$export$isSpectrumList, TRUE)
+            expect_equal(exp$export$numSpec, 88)
+            fs::dir_delete("Curve")
+            fs::file_delete("settings.csv")
+            fs::file_delete("testdata_bruker.zip")
+          })
+
