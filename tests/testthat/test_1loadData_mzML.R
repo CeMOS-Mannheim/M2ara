0c41d19a4ecdbe5c9090fcea07f6eb615c4e976b
thomas-enzlein <thomas.enzlein@gmail.com>
2024-08-12 07:44:08 +0200

    - another try to fix tests

--- tests/testthat/test_1loadData_mzML.R
+++ tests/testthat/test_1loadData_mzML.R
@@ -3,67 +3,84 @@ library(testthat)
 test_that("data can be loaded and processed from mzML",
           {
 
-            timeNow <- function() {
-              format(Sys.time(), "%H:%M:%OS")
+
+            infoStateChecker <- function(App, reachInfoState, maxCounter = 10) {
+              counter <- 0
+              while(!App$get_values(export = TRUE)$export$infoState == reachInfoState)
+              {
+                Sys.sleep(30)
+                counter <- counter + 1
+
+                if(counter>=maxCounter) {
+                  stop("infoState did not reach:", reachInfoState, "\n",
+                       "Last infoState was: ", App$get_values(export = TRUE)$export$infoState,
+                       ".\n")
+                }
+              }
             }
-            setwd(dirname(common::file.find(pattern = "req.txt")))
 
-            # download test data and unzip
-            curl::curl_download("https://figshare.com/ndownloader/files/46156788", "testdata_mzML.zip")
-            unzip("testdata_mzML.zip")
+              timeNow <- function() {
+                format(Sys.time(), "%H:%M:%OS")
+              }
+              setwd(dirname(common::file.find(pattern = "req.txt")))
 
-            fs::file_delete("testdata_mzML.zip")
+              # download test data and unzip
+              curl::curl_download("https://figshare.com/ndownloader/files/46156788", "testdata_mzML.zip")
+              unzip("testdata_mzML.zip")
 
-            # overwrite settings
-            if(!file.exists("tests/testthat/settings_mzML_data.csv")) {
-              stop("Could not find settings_mzML_data.csv.\n")
-            }
-            fs::file_copy(path = "tests/testthat/settings_mzML_data.csv",
-                          new_path = "settings.csv",
-                          overwrite = TRUE)
+              fs::file_delete("testdata_mzML.zip")
 
-            library(shinytest2)
-            options(shiny.testmode = TRUE)
+              # overwrite settings
+              if(!file.exists("tests/testthat/settings_mzML_data.csv")) {
+                stop("Could not find settings_mzML_data.csv.\n")
+              }
+              fs::file_copy(path = "tests/testthat/settings_mzML_data.csv",
+                            new_path = "settings.csv",
+                            overwrite = TRUE)
 
-            cat("\n", timeNow(), "Starting app...\n")
-            app <- AppDriver$new(app_dir = getwd(),
-                                 name = "M2ara_mzML load test",
-                                 seed = 42,
-                                 timeout = 10000,
-                                 load_timeout = 30*1000,
-                                 wait = TRUE)
+              library(shinytest2)
+              options(shiny.testmode = TRUE)
 
+              cat("\n", timeNow(), "Starting app...\n")
+              app <- AppDriver$new(app_dir = getwd(),
+                                   name = "M2ara_mzML load test",
+                                   seed = 42,
+                                   timeout = 10000,
+                                   load_timeout = 30*1000,
+                                   wait = TRUE)
 
-            app$wait_for_idle()
-            Sys.sleep(5)
-            cat(timeNow(), "App started. Loading data...\n")
-            app$click("load")
-            app$wait_for_idle(timeout = 300*1000)
-            Sys.sleep(60)
-            cat(timeNow(), "Start processing...\n")
-            app$click("process")
-            app$wait_for_idle(timeout = 300*1000)
-            cat(timeNow(), "Processing done.\n")
+              app$wait_for_idle()
+              Sys.sleep(5)
+              cat(timeNow(), "App started. Loading data...\n")
+              app$click("load")
+              infoStateChecker(app, "loaded")
+              app$wait_for_idle(timeout = 300*1000)
 
-            app$click("doPca")
-            app$wait_for_idle()
+              cat(timeNow(), "Start processing...\n")
+              app$click("process")
+              infoStateChecker(app, "processed")
+              app$wait_for_idle(timeout = 300*1000)
+              cat(timeNow(), "Processing done.\n")
 
-            app$click("doClust")
-            app$wait_for_idle()
+              app$click("doPca")
+              app$wait_for_idle()
 
-            exp <- app$get_values(export = TRUE)
+              app$click("doClust")
+              app$wait_for_idle()
 
-            expect_equal(exp$export$isSpectrumList, TRUE)
-            expect_equal(exp$export$numSpec, 144)
+              exp <- app$get_values(export = TRUE)
 
-            expect_equal(exp$export$infoState, "processed")
+              expect_equal(exp$export$isSpectrumList, TRUE)
+              expect_equal(exp$export$numSpec, 144)
 
-            expect_true(is_tibble(exp$export$pca$scores))
-            expect_true(is_tibble(exp$export$pca$loadings))
+              expect_equal(exp$export$infoState, "processed")
 
-            expect_true(latrend::is.lcModels(exp$export$clust))
-            expect_true(is_tibble(extractLaClusters(exp$export$clust)))
-            fs::file_delete("settings.csv")
-            fs::dir_delete("mzML")
-          })
+              expect_true(is_tibble(exp$export$pca$scores))
+              expect_true(is_tibble(exp$export$pca$loadings))
+
+              expect_true(latrend::is.lcModels(exp$export$clust))
+              expect_true(is_tibble(extractLaClusters(exp$export$clust)))
+              fs::file_delete("settings.csv")
+              fs::dir_delete("mzML")
+            })
 
